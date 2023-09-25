962b53544980b518dea194a8d9680094579aadf0
juanmoli339 <rivalmoli3n_23@hotmail.com>
2023-09-25 21:27:10 +0200

    upload files

---
+++ 01-Ramp_Up/02-Python/Ejercicios.py
@@ -0,0 +1,25 @@
+
+# EJERCICIO 1
+nombre = "Juan" 
+apellidos = "Moliner Vicente"
+edad = 42
+edad = 32
+
+#print(nombre)
+#print(apellidos)
+#print(edad)
+
+
+# EJERCICIO 2
+variable_string = "Cazador 'cazado'"
+print (variable_string)
+print (type(variable_string))
+otro_string = "dijo el posadero"
+print (variable_string + otro_string)
+variable_entera = 8
+print (variable_entera)
+variable_float = float(variable_entera)
+print(type(variable_float))
+
+# EJERCICIO 3
+
